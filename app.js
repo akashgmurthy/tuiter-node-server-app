5544b8ad50d0497ca4ce77410f0b8d93ffa0fa23
Akash Ganesamurthy <akashganesamurthy@Akashs-MacBook-Air.local>
2023-04-02 21:25:10 -0400

    node code

---
+++ app.js
@@ -0,0 +1,12 @@
+import express from 'express'
+import cors from 'cors'
+import HelloController from "./controllers/hello-controller.js"
+import UserController from './controllers/users/users-controller.js'
+import tuitsController from './controllers/tuits/tuits-controller.js'
+const app = express()
+app.use(cors())
+app.use(express.json());
+tuitsController(app)
+HelloController(app)
+UserController(app)
+app.listen(process.env.PORT || 4000)
